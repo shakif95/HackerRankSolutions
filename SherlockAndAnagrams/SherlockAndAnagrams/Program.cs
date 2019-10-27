4b11eceb60da3d696983dc339c136b0595699b8f
shakif95 <shakif.malek@gmail.com>
2019-10-28 00:15:52 +0600

    First Commit

---
+++ SherlockAndAnagrams/SherlockAndAnagrams/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SherlockAndAnagrams
+{
+    class Program
+    {
+       
+        // Complete the sherlockAndAnagrams function below.
+        static int SherlockAndAnagrams(string s)
+        {
+            Dictionary<string, int> counts = new Dictionary<string, int>(26);
+
+            for (int start = 0; start < s.Length; start++)
+            {
+                string substring = "";
+                for (int end = start; end < s.Length; end++)
+                {
+                    string c = s[end].ToString();
+                    substring += c;
+
+                    if (substring.Length > 1)
+                    {
+                        char[] array = substring.ToCharArray();
+                        Array.Sort(array);
+                        substring = new string(array);
+                    }
+
+
+                    if (counts.ContainsKey(substring))
+                    {
+                        counts[substring]++;
+                    }
+                    else
+                    {
+                        counts.Add(substring,1);
+                    }
+                }
+            }
+            int analgrams = 0;
+            foreach(int value in counts.Values)
+            {
+                analgrams += (value*(value-1)) / 2;
+            }
+
+            return analgrams;
+        }
+
+        static void Main(string[] args)
+        {
+            int test = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+
+            while(i<test)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine(SherlockAndAnagrams(s));
+                i++;
+            }
+
+            
+
+            Console.Read();
+        }
+    }
+}
---
+++ SpecialPalindormeAgain/SpecialPalindormeAgain/Program.cs
@@ -0,0 +1,45 @@
+﻿// Date Sunmitted: 
+// Problem Link: https://www.hackerrank.com/challenges/special-palindrome-again/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+//Problem Type: string
+using System;
+
+namespace SpecialPalindormeAgain
+{
+    class Program
+    {
+        static long substrCount(int n, string s)
+        {
+            long result = 0;
+            char[] allchars = s.ToCharArray();
+            char[] subChar = new char[s.Length + 1];
+            int subcharlen = 0;
+            char first = allchars[0];
+            for(int i=1;i<allchars.Length;i++)
+            {
+                if(allchars[i]==first)
+                {
+                    subChar[subcharlen] = allchars[i];
+                    subcharlen++;
+                }
+                else
+                {
+                    if (subChar == s.Substring(i, s.Length).ToCharArray())
+                        return 1;
+                }
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            long result = substrCount(n, s);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
